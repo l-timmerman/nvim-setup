7d9f128eba5297ffee03b4b87c5e1204d6029c16
Lars Timmerman <infigoforza@gmail.com>
2025-04-23 21:47:30 +0200

    Remove redundandt lua LSP
    For now only lua formatting is needed, so the lsp is for now overkill

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,9 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", 
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
-      })
-    end
-  },
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 }
