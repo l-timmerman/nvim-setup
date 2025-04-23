11f7cee5b160c99a9e512e50fa7ed1b6b1009519
Lars Timmerman <infigoforza@gmail.com>
2025-04-23 20:53:08 +0200

    Add none ls with lua formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"nvimtools/none-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local none_ls = require("null-ls")
+
+			none_ls.setup({
+				sources = {
+					none_ls.builtins.formatting.stylua, -- add Lua formatter
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+}
