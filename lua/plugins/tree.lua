666da6bd4a350ddde9f94beae30235e2c3936fa7
Lars Timmerman <infigoforza@gmail.com>
2025-04-23 21:53:23 +0200

    Apply format with stylua

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,22 +1,22 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {
-      vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true }),
-      vim.keymap.set("n", "<leader>ft", ":NvimTreeFocus<CR>", { silent = true }),
-      update_focused_file = {
-        enable = true,
-        update_root = {
-          enable = false,
-          ignore_list = {},
-        },
-        exclude = false,
-      },
-    }
-  end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true }),
+			vim.keymap.set("n", "<leader>ft", ":NvimTreeFocus<CR>", { silent = true }),
+			update_focused_file = {
+				enable = true,
+				update_root = {
+					enable = false,
+					ignore_list = {},
+				},
+				exclude = false,
+			},
+		})
+	end,
 }
