ccb5f6ef553abc77c17b4e77225b5e81d3beb4cb
Lars Timmerman <infigoforza@gmail.com>
2025-04-23 20:43:48 +0200

    Add focus tree keyinding and focus active file
    Add focus tree keybdingin and focus active

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -7,7 +7,16 @@ return {
   },
   config = function()
     require("nvim-tree").setup {
-         vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true }),
+      vim.keymap.set("n", "<leader>ft", ":NvimTreeFocus<CR>", { silent = true }),
+      update_focused_file = {
+        enable = true,
+        update_root = {
+          enable = false,
+          ignore_list = {},
+        },
+        exclude = false,
+      },
     }
   end,
 }
